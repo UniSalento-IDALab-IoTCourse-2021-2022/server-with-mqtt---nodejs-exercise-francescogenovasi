a9746a61c7aeb17018d63c51ea043a8388e628c2
Francesco Genovasi <francesco.genovasi@studenti.unisalento.it>
2022-07-11 16:57:50 +0200

    mqtt ok 2

--- serverApp.js
+++ serverApp.js
@@ -1,11 +1,9 @@
 ﻿const express = require("express");
 const WebSocket = require('ws');
 const path = require('path');
-const mongodb = require('mongodb');
+const mqtt=require('mqtt');
 
-const MongoClient = mongodb.MongoClient;
 const app = express();
-const uri = 'mongodb://localhost/';
 
 const wss = new WebSocket.Server({ port: 3001 });
 
@@ -16,47 +14,26 @@ wss.on('connection', function connection(ws) {
     ws.send('something');
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+var mqttClient = mqtt.connect("mqtt://mqtt.eclipseprojects.io",{clientId:"mqttjs041"});
+
+mqttClient.on("connect",function(){
+    console.log("connected");
 });
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-)
 
-app.use(express.json());
-app.post("/temperature", (req, res, next) => {
-    console.log(req.body.temperature);
-    var temperature = req.body.temperature;
-    var timestamp = req.body.timestamp;
-    var sensor = req.body.sensor;
+mqttClient.on("error",function(error){
+    console.log("Can't connect" + error);
+});
 
-    async function pushInDb() {
+mqttClient.on('message',function(topic, message, packet){ //listener
 
-        const client = new MongoClient(uri, {useUnifiedTopology: true});
-        try {
+    console.log("message is " + message);
+    console.log("topic is " + topic);
 
-            await client.connect();
+    var messageJSON = JSON.parse(message);
+    var temperature = messageJSON.temperature;
+    var timestamp = messageJSON.timestamp;
+    var sensor = messageJSON.sensor;
 
-            const database = client.db("TemperatureDB");
-            const temperatureColl = database.collection("temperature");
-            // create a document to be inserted
-            const doc = {
-                value: temperature,
-                timestamp: timestamp,
-                sensorId: sensor,
-                roomId: 'room1'
-            };
-
-            const result = await temperatureColl.insertOne(doc);
-            console.log(`${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,);
-        } finally {
-            await client.close();
-        }
-    }
-
-    pushInDb().catch(console.dir);
     async function pushToClient(){
         wss.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
@@ -65,41 +42,24 @@ app.post("/temperature", (req, res, next) => {
         });
     }
     pushToClient().catch(console.dir);
-    res.sendStatus(200)
 });
 
-app.get('/dashboard', async (req, res) => {
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
 
-    /*
-    async function run() {
-        const client = new MongoClient(uri, {useUnifiedTopology: true});
-        try {
-            await client.connect();
-            const database = client.db("TemperatureDB");
-            const tem = database.collection("temperature");
-            // Query for a temperature with a timestamp that is greater than 0
-            const query = { timestamp: {$gt: 0}};
-            const options = {
-                // sort matched documents in descending order by timestamp
-                sort: { timestamp: -1 },
-            };
-            const singleTemperature = await tem.findOne(query, options);
-            // since this method returns the matched document, not a cursor, print it directly
-            console.log(singleTemperature);
-            try {
-                return singleTemperature.value;
-            }
-            catch (e)
-            {
-                return -1;
-            }
-        } finally {
-            await client.close();
-        }
-    }
-    //use await for wating the promise
-    var finalTemp = await run().catch(console.dir);
-    res.send('Hello World! The last temperature is: '+finalTemp);
-     */
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json());
+
+app.get('/dashboard', async (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
+
+var topic="trial1/IoTLesson/raspberrypy1/temperature";
+console.log("subscribing to topic" + topic);
+mqttClient.subscribe(topic); //single topic
